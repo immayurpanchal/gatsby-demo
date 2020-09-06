11849a8f04e4d7387a42771217275f7189801e9e
Mayur Panchal <mayurpanchal002@gmail.com>
2020-09-06 22:20:34 +0530

    created demo route using page-3

---
+++ src/pages/page-3.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const ThirdPage = () => (
+  <Layout>
+    <h1>Hi from the third page</h1>
+    <p>Welcome to page 3</p>
+  </Layout>
+)
+
+export default ThirdPage
